743704cad00bb87d45e97a436cc49e9d18577f2b
mummpl <mummpl@gmail.com>
2018-05-02 22:53:51 +0200

    Added universe builder mode with the ability to draw patterns by hand and simulate the universe

--- game_of_life.py
+++ game_of_life.py
@@ -24,6 +24,8 @@ blocksize = size[0] / gridSize
 
 universe = []
 
+lastSavedUniverse = ["nothing saved here"]
+
 # randomGridInitializer
 for column in range(gridSize):
     tempcolumn = []
@@ -175,28 +177,19 @@ while randomGame is True:
     # bring new universe into place
     universe = tempUniverse
 
-    pygame.event.get()
-
     clock.tick(fps)
     pygame.display.flip()
 
 # Builder module
 while builder is True:
 
-    print("buildermode activated")
-
     # initialization
     clock = pygame.time.Clock()
 
-    ev = pygame.event.get()
-
-    for event in ev:
-        if event.type == pygame.QUIT:
-            sys.exit()
-        if pygame.key.get_pressed()[pygame.K_ESCAPE]:
-            sys.exit()
-
     screen.fill(black)
+
+    simulationMode = False
+    drawMode = True
 
     # initialize empty universe
     universe = []
@@ -206,19 +199,149 @@ while builder is True:
             tempColumn.append(0)
         universe.append(tempColumn)
 
-    # draw the latest state
-    for column in range(gridSize):
-        for row in range(gridSize):
-            rect = pygame.Rect(column * blocksize, row * blocksize, blocksize, blocksize)
-            if universe[column][row] == 1:
-                pygame.draw.rect(screen, white, rect)
-            else:
-                pygame.draw.rect(screen, black, rect)
+    # draw mode
+    while drawMode is True:
 
+        # initialization
+        clock = pygame.time.Clock()
 
+        if lastSavedUniverse != ["nothing saved here"]:
+            universe = lastSavedUniverse
 
+        # draw the latest state
+        for column in range(gridSize):
+            for row in range(gridSize):
+                rect = pygame.Rect(column * blocksize, row * blocksize, blocksize, blocksize)
+                if universe[column][row] == 1:
+                    pygame.draw.rect(screen, white, rect)
+                else:
+                    pygame.draw.rect(screen, black, rect)
 
-    pygame.event.get()
+        # handle user interaction
+        for event in pygame.event.get():
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+
+                for column in range(gridSize):
+                    for row in range(gridSize):
+                        rect = pygame.Rect(column * blocksize, row * blocksize, blocksize, blocksize)
+                        if rect.collidepoint(pos[0], pos[1]):
+                            if universe[column][row] == 0:
+                                universe[column][row] = 1
+                            else:
+                                universe[column][row] = 0
+
+            # switching into simulation mode
+            if pygame.key.get_pressed()[pygame.K_RETURN]:
+                lastSavedUniverse = universe
+                drawMode = False
+                simulationMode = True
+
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if pygame.key.get_pressed()[pygame.K_ESCAPE]:
+                sys.exit()
+
+        clock.tick(fps)
+        pygame.display.flip()
+
+    while simulationMode is True:
+
+        # initialization
+        clock = pygame.time.Clock()
+
+        # draw the latest state
+        for column in range(gridSize):
+            for row in range(gridSize):
+                rect = pygame.Rect(column * blocksize, row * blocksize, blocksize, blocksize)
+                if universe[column][row] == 1:
+                    pygame.draw.rect(screen, white, rect)
+                else:
+                    pygame.draw.rect(screen, black, rect)
+
+        # update the state
+        tempUniverse = []
+
+        for column in range(gridSize):
+            tempColumn = []
+            for row in range(gridSize):
+                tempColumn.append(0)
+            tempUniverse.append(tempColumn)
+
+        # applying game rules
+        for column in range(gridSize):
+            for row in range(gridSize):
+                # counting
+                counter = 0
+
+                # above
+                if row > 0:
+                    if universe[column][row - 1] == 1:
+                        counter += 1
+
+                # below
+                if row < gridSize - 1:
+                    if universe[column][row + 1] == 1:
+                        counter += 1
+
+                # right
+                if column < gridSize - 1:
+                    if universe[column + 1][row] == 1:
+                        counter += 1
+
+                # left
+                if column > 0:
+                    if universe[column - 1][row] == 1:
+                        counter += 1
+
+                # above_right
+                if row > 0 and column < gridSize - 1:
+                    if universe[column + 1][row - 1] == 1:
+                        counter += 1
+
+                # above_left
+                if row > 0 and column > 0:
+                    if universe[column - 1][row - 1] == 1:
+                        counter += 1
+
+                # below_right
+                if row < gridSize - 1 and column < gridSize - 1:
+                    if universe[column + 1][row + 1] == 1:
+                        counter += 1
+
+                # below_left
+                if row < gridSize - 1 and column > 0:
+                    if universe[column - 1][row + 1] == 1:
+                        counter += 1
+
+                # thumbs up or down
+                if universe[column][row] == 0:
+                    if counter == 3:
+                        tempUniverse[column][row] = 1
+                if universe[column][row] == 1:
+                    if counter != 2 and counter != 3:
+                        tempUniverse[column][row] = 0
+                    else:
+                        tempUniverse[column][row] = 1
+
+        # bring new universe into place
+        universe = tempUniverse
+
+        # handle user interaction
+        for event in pygame.event.get():
+
+            if pygame.key.get_pressed()[pygame.K_RETURN]:
+                drawMode = True
+                simulationMode = False
+
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if pygame.key.get_pressed()[pygame.K_ESCAPE]:
+                sys.exit()
+
+        clock.tick(fps)
+        pygame.display.flip()
 
     clock.tick(fps)
     pygame.display.flip()
